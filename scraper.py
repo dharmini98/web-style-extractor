327bae0a39df9564f9fc9bc7df29eeea164842c9
Dharmini Saravanan <dharmini1998@gmail.com>
2024-09-02 22:41:31 -0700

    Added external css style

--- scraper.py
+++ scraper.py
@@ -16,12 +16,12 @@ import json
 import cssutils
 import argparse
 
-
+""""
 parser=argparse.ArgumentParser(description='extract website style')
 parser.add_argument('url',help='url of the website')
 parser.add_argument('-O','--output', default='website_styles.json',help='output of the json file')
 args= parser.parse_args()
-
+"""
 #selenium with headless chrome
 options=Options()
 options.add_argument('--headless')
@@ -72,5 +72,57 @@ def style(soup): #embedded and inline
     
     return styles
   
-#def external_css(soup, url): #extracts external style
-#    for link in soup.find_all('link', rel='stylesheet'):
+def external_css(soup, url): #extracts external style css
+    styles={}
+    try:
+       for link in soup.find_all('link', rel='stylesheet'):
+          css_url=link.get('href')
+          if not css_url.startswith('http'):
+             css_url = f'{url.rstrip('/')}/{css_url.lstrip('/')}'
+          if css_url not in stylesheet_cache:
+             response=sessions.get(css_url)
+             if response.status_code==200:
+                stylesheet_cache[css_url]=response.text
+             else:
+                pass
+                continue
+          sheet=cssutils.parseString(stylesheet_cache[css_url]) 
+          for i in sheet:
+             if i.style==i.STYLE_RULE:
+                selector=i.selectorText
+                property=i.style.cssText
+                for element in soup.select(selector):   
+                        t=str(element)
+                        if t not in styles:                 
+                           styles[t]=property
+                        else:
+                           styles[t]+=';'+property           
+
+    except Exception as e:
+        logging.warning(f'Error2:{e}')
+   
+    return styles
+
+def main():
+   url='https://ridge.com'
+   #output=args.output
+   soup= scrape(url)
+   if soup is None:
+      print("Failed to scrape the website")
+
+   inline_embedded=style(soup)
+   external_style= external_css(soup, url)
+   all_styles={**inline_embedded, **external_style}
+   """
+   with open(output, 'w') as f:
+      json.dump(all_styles,f,indent=2) 
+   print(f"Extracted styles: {output}")
+   """
+   print(f"Extracted styles: {all_styles}")
+   driver.close()
+
+   if __name__=="__main__":
+      main()
+
+
+       
